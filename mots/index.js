35ab022b76fe29ed6178988d856b284801a37b00
napoleonischeKatzen <lcreese04@gmail.com>
2024-12-24 00:37:42 -0600

    Update and rename tableaux.js to index.js

---
+++ mots/index.js
@@ -0,0 +1,12 @@
+const verbs = [
+    { language: 'classical-latin', word: 'esse' },
+    { language: 'french', word: 'être'},
+    { language: 'classical-latin', word: 'posse'},
+    { language: 'german', word: 'sein'}
+]
+const nouns = [
+    { language: 'classical-latin', word: 'deus'}
+]
+const adjectives = [
+    { language: 'classical-latin', word: 'unus'},
+]
